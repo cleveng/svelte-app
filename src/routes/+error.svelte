639b14fdd85bcd1def16ba6d8bda01a27a92053c
cleveng <cleveng@gmail.com>
2026-01-14 14:45:06 +0800

    perf: add error.svelte

---
+++ src/routes/+error.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { resolve } from '$app/paths'
+</script>
+
+<div class="flex h-screen w-screen items-center justify-center">
+  <div class="flex flex-col items-center justify-center text-sm max-md:px-4">
+    <h1 class="text-8xl font-bold text-indigo-500 md:text-9xl">404</h1>
+    <div class="my-5 h-1 w-16 rounded bg-indigo-500 md:my-7"></div>
+    <p class="text-2xl font-bold text-gray-800 md:text-3xl">Page Not Found</p>
+    <p class="mt-4 max-w-md text-center text-sm text-gray-500 md:text-base">
+      The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.
+    </p>
+    <div class="mt-6 flex items-center gap-4">
+      <a
+        href={resolve('/')}
+        class="rounded-md bg-gray-800 px-7 py-2.5 text-white transition-all hover:bg-black active:scale-95"
+      >
+        Return Home
+      </a>
+      <a
+        href={resolve('/')}
+        class="rounded-md border border-gray-300 px-7 py-2.5 text-gray-800 transition-all active:scale-95"
+      >
+        Contact support
+      </a>
+    </div>
+  </div>
+</div>
