63dcf0e0a012f72b3c363e3f592a4231882b8c04
cleveng <cleveng@gmail.com>
2026-01-31 16:52:06 +0800

    perf: disabled console debugger for production

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,12 @@ import { sveltekit } from '@sveltejs/kit/vite'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
-export default defineConfig({ plugins: [tailwindcss(), enhancedImages(), sveltekit()] })
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [tailwindcss(), enhancedImages(), sveltekit()],
+    esbuild: {
+      // 生产环境下禁用console和debugger
+      drop: mode === 'production' ? ['console', 'debugger'] : [],
+    },
+  }
+})
