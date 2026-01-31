3b24103898f2c2fb3dc5f879a8824fb88f94b919
cleveng <cleveng@gmail.com>
2026-01-31 17:23:52 +0800

    perf: wip

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,28 @@
 <script lang="ts">
   import { Toaster } from 'svelte-sonner'
 
-  import './layout.css'
+  import { page } from '$app/state'
+
   import favicon from '$lib/assets/favicon.svg'
 
+  import { appStore } from '$lib/stores/app.store'
+
+  import './layout.css'
+
   let { children } = $props()
+
+  if (!page.data?.token) {
+    appStore.update(state => ({
+      ...state,
+      loggedIn: false,
+      token: null,
+    }))
+  }
 </script>
 
 <svelte:head>
   <link rel="icon" href={favicon} />
-  <title>首 页</title>
+  <title>首页</title>
 </svelte:head>
 
 <Toaster position="top-center" richColors />
